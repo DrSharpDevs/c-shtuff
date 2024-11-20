e5894956b511f7a966d5d7f22bab0c1aee08ebb0
DrSharpDevs <156481792+DrSharpDevs@users.noreply.github.com>
2024-11-20 09:35:21 -0500

    Add files via upload

---
+++ CS0101_Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CS0101_Strings
+{
+    internal partial class Program
+    {
+        // In a console application, the program starts at the Main method.
+        static void Main(string[] args)
+        {
+            // Check out Strings1.cs and complete the tasks there.
+            Strings1();
+
+            // Check out Strings2.cs and complete the tasks there.
+            //Strings2();
+
+            // Complete the MadLibs method in MadLib.cs
+            //Console.WriteLine(MadLib());
+
+            // OPTIONAL: Check out Challenges.cs and complete the tasks there.
+
+        }
+
+
+
+
+
+    }
+}
+
